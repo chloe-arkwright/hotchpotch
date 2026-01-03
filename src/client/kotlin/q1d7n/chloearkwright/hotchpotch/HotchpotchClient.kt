355b609d7ba47f2671e2533a61f2e2847c28f8f9
Chloe Arkwright <chloe@ellie.moi>
2026-01-03 11:08:58 +0000

    Import fabric mod template with minor tweak to build.yml.

---
+++ src/client/kotlin/q1d7n/chloearkwright/hotchpotch/HotchpotchClient.kt
@@ -0,0 +1,9 @@
+package q1d7n.chloearkwright.hotchpotch
+
+import net.fabricmc.api.ClientModInitializer
+
+object HotchpotchClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/q1d7n/chloearkwright/hotchpotch/HotchpotchDataGenerator.kt
@@ -0,0 +1,9 @@
+package q1d7n.chloearkwright.hotchpotch
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object HotchpotchDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
