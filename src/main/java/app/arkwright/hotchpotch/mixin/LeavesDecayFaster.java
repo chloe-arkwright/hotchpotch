63496c6a5f660143f3d16e3eea32b6493e4e468e
Chloe Arkwright <chloe@ellie.moi>
2026-01-20 07:38:22 +0000

    Seemed to miss exactly 1 import...

--- src/main/java/app/arkwright/hotchpotch/mixin/LeavesDecayFaster.java
+++ src/main/java/app/arkwright/hotchpotch/mixin/LeavesDecayFaster.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import q1d7n.chloearkwright.hotchpotch.LeafDecayManager;
+import app.arkwright.hotchpotch.LeafDecayManager;
 import app.arkwright.hotchpotch.registration.ModGameRules;
 
 @Mixin(LeavesBlock.class)
